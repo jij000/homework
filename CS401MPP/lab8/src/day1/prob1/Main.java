1f4e0413298e9fe04bb2c1343685f299bf7ddda6
jij000 <jij000@gmail.com>
2019-06-11 14:03:02 -0500

    day1 prob1

---
+++ CS401MPP/lab8/src/day1/prob1/Main.java
@@ -0,0 +1,19 @@
+package day1.prob1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BiFunction<Double, Double, List<Double>> bi = (x, y) -> {
+			List<Double> list = new ArrayList<>();
+			list.add(Math.pow(x, y));
+			list.add(x * y);
+			return list;
+		};
+		System.out.println(bi.apply(2.0, 3.0));
+	}
+
+}
