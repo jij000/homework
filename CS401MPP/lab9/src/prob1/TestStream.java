992e0684919c4aba1e28c286b5821d410c7fde69
jiangyj <jij000@gmail.com>
2019-06-13 18:06:48 -0500

    prob1 v2

--- CS401MPP/lab9/src/prob1/TestStream.java
+++ CS401MPP/lab9/src/prob1/TestStream.java
@@ -1,11 +1,12 @@
 package prob1;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestStream {
 
 	public static void main(String[] args) {
-		System.out.println(Stream.of(new String[] { "Bill", "Thomas", "Mary" }).reduce((a, b) -> a + ", " + b).get());
+		System.out.println(Stream.of(new String[] { "Bill", "Thomas", "Mary" }).collect(Collectors.joining(", ")));
 	}
 
 }
