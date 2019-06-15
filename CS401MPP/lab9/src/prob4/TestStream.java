44fa0665a83ec16b940fa3e60deedeb63e8de30b
jiangyj <jij000@gmail.com>
2019-06-15 18:55:16 -0500

    prob4 minor change

--- CS401MPP/lab9/src/prob4/TestStream.java
+++ CS401MPP/lab9/src/prob4/TestStream.java
@@ -6,11 +6,9 @@ public class TestStream {
 
 	public static void printSquares(int num) {
 
-		IntStream.iterate(1, x -> {
-				x = (int) Math.sqrt(x) + 1;
-				return x * x;
-			})
+		IntStream.iterate(1, x -> x + 1)
 			.limit(num)
+			.map(x -> x * x)
 			.forEach(System.out::println);
 	}
 	
