6e62139a933d2ce7e430af26475b70edb8ef4e1a
jiangyj <jij000@gmail.com>
2019-06-16 18:50:33 -0500

    prob2

---
+++ CS401MPP/lab10/src/prob2/FindInList.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package prob2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Create a generic programming solution to the problem of finding the second
+ * smallest element in a list. In other words, devise a public static method
+ * secondSmallest so that it can handle the biggest possible range of types.
+ * 
+ * @author jiang
+ *
+ */
+public class FindInList {
+
+	public static <T extends Comparable<? super T>> T secondSmallest(List<? extends T> list) {
+		if (list.size() < 2) {
+			return null;
+		}
+		T min;
+		T secondMin;
+		// initialize
+		if (list.get(0).compareTo(list.get(1)) > 0) {
+			min = list.get(1);
+			secondMin = list.get(0);
+		} else {
+			min = list.get(0);
+			secondMin = list.get(1);
+		}
+		for (T t : list) {
+			if (min.compareTo(t) > 0 && secondMin.compareTo(t) > 0) { // min > t && secondMin > t
+				secondMin = min;
+				min = t;
+			}
+			if (min.compareTo(t) < 0 && secondMin.compareTo(t) > 0) { // min < t && secondMin > t
+				secondMin = t;
+			}
+//			if (min.compareTo(t) < 0 && secondMin.compareTo(t) < 0) { // min < t && secondMin < t
+				// do nothing;
+//			}
+		}
+		return secondMin;
+//		Collections.sort(list);
+//		return list.get(1);
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		List<Double> dList = Arrays.asList(2.3,5.9,1.2,2.1,2.0,4.8,5.7);
+		Double d = secondSmallest(dList);
+		System.out.println(d);
+		
+		List<Integer> iList = Arrays.asList(2,5,1,2,3,4,5);
+		Integer i = secondSmallest(iList);
+		System.out.println(i);
+		
+		List<String> strList = Arrays.asList("ddd", "aaa", "bbb", "ccc");
+		String str = secondSmallest(strList);
+		System.out.println(str);
+	}
+
+}
