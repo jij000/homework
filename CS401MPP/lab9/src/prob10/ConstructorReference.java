fa4f8dcaba51d763a8e1d406f4dcbcade904ad9c
jiangyj <jij000@gmail.com>
2019-06-15 22:30:50 -0500

    prob10

--- CS401MPP/lab9/src/prob10/ConstructorReference.java
+++ CS401MPP/lab9/src/prob10/ConstructorReference.java
@@ -1,11 +1,8 @@
 package prob10;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
+
+import prob7b.TriFunction;
 
 class Human
 {
@@ -55,12 +52,19 @@ public static void main(String args[]){
 	Human[] list = { new Human("Joe",35,"Male"), new Human("Jane",45,"Female"), new Human("John",30,"Male")};
 	
     // Query 1  : Print only Female canditates names
-	
+	Stream.of(list).filter(x->x.getGender().equals("Female")).map(x->x.getName()).forEach(System.out::println);
 
     // Query 2 : Cretae an objecy by choosing suitable Interface to the specified constructors(Totally 3 constuctors)using fouth type of Method Reference ClassName::new. Then print the object status 
- 
+	TriFunction<String, Integer, String, Human> fun = Human::new;
+	System.out.println(fun.apply("JJJJ", 88, "Male"));
+	
 	// Query 3 : Count the male candidates whose age is more than 30
-    
+	System.out.println(Stream.of(list)
+		.filter(x->x.getGender().equals("Male"))
+		.filter(x->x.getAge() > 30)
+		.count()
+	);
+
     
     
    }
