e433c2aae451dd4bece0c59b1e030912253fe76d
jiangyj <jij000@gmail.com>
2019-07-01 14:59:55 -0500

    finish

--- CS544EA/lab7/Lab7Optimization/src/main/java/edu/mum/cs544/App.java
+++ CS544EA/lab7/Lab7Optimization/src/main/java/edu/mum/cs544/App.java
@@ -2,10 +2,7 @@ package edu.mum.cs544;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 
 public class App {
 
@@ -13,12 +10,25 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         emf = Persistence.createEntityManagerFactory("cs544");
-
+        // start time
         long start = System.nanoTime();
+
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
+//        TypedQuery<Owner> query = em.createQuery("from Owner", Owner.class);
+
+        // d) Remove the sub-select strategy and use a join fetch query in
+        //App.java to retrieve everything. Also check the difference between us-
+        //ing a named query, or just a query directly in code.
+//        TypedQuery<Owner> query = em.createQuery("from Owner o join fetch o.pets ", Owner.class);
+        // [OUTPUT]>>> : To fetch this data from the database took 20074 milliseconds.
+        // e) Lastly modify the application to use an Entity Graph instead of a join fetch.
+        EntityGraph<Owner> graph = em.createEntityGraph(Owner.class);
+        graph.addSubgraph("pets");
         TypedQuery<Owner> query = em.createQuery("from Owner", Owner.class);
+        query.setHint("javax.persistence.fetchgraph", graph);
+        // [OUTPUT]>>> : To fetch this data from the database took 38059 milliseconds.
         List<Owner> ownerlist = query.getResultList();
         for (Owner o : ownerlist) {
             o.getPets().size();
--- CS544EA/lab7/Lab7Optimization/src/main/java/edu/mum/cs544/Owner.java
+++ CS544EA/lab7/Lab7Optimization/src/main/java/edu/mum/cs544/Owner.java
@@ -1,5 +1,7 @@
 package edu.mum.cs544;
 
+import org.hibernate.annotations.*;
+
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -18,6 +20,18 @@ public class Owner {
     private String name;
 	@OneToMany (cascade={CascadeType.PERSIST})
 	@JoinColumn (name="clientid")
+	// a) Add the @LazyCollection with option EXTRA to the association and run App again.
+	// [OUTPUT]>>> : To fetch this data from the database took 51103 milliseconds.
+//	@LazyCollection(LazyCollectionOption.EXTRA)
+	// b) Remove the @LazyCollection, and modify the mapping for Owner.java to
+	//use batch fetching, batch size 10. Also check the time when using sizes 5
+	//and 50.
+	// [OUTPUT]>>> : To fetch this data from the database took 35596 milliseconds.
+	// [OUTPUT, when size = 50]>>> : To fetch this data from the database took 33670 milliseconds.
+//	@BatchSize(size=10)
+	// c) Modify the mapping to use the sub-select strategy instead of batch fetching.
+	// [OUTPUT]>>> : To fetch this data from the database took 18575 milliseconds.
+//	@Fetch(FetchMode.SUBSELECT)
     private List<Pet> pets;
     
 	public Owner() {
