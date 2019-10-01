9078394f4d49b02c8c1ed6b85ad377860b9622f7
jiangyj <jij000@gmail.com>
2019-09-30 22:44:16 -0500

    Lab01-2

---
+++ CS545WAA/Starbucks/src/java/edu/mum/cs/listener/StarbucksContextListener.java
@@ -0,0 +1,28 @@
+package edu.mum.cs.listener;
+
+import edu.mum.cs.data.DataFacade;
+import edu.mum.cs.data.DataFacadeFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class StarbucksContextListener implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent event) {
+
+        ServletContext servletContext = event.getServletContext();
+
+        // Set up Database access
+        String version = servletContext.getInitParameter("version");
+        DataFacade data = (new DataFacadeFactory()).getDataFacadeInstance(version);
+
+        servletContext.setAttribute("dataSource", data);
+
+    }
+
+    public void contextDestroyed(ServletContextEvent event) {
+    }
+}
