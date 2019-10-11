da55707c190447b49bc0b353cdcd17008d3bb379
Yanjun jiang <jij000@gmail.com>
2019-10-10 23:30:40 -0500

    Lab08

---
+++ CS545WAA/Lab08/employeevaliddemo/src/main/java/edu/mum/cs/employeevaliddemo/config/MyWebMvcConfigurer.java
@@ -0,0 +1,41 @@
+package edu.mum.cs.employeevaliddemo.config;
+
+import edu.mum.cs.employeevaliddemo.interceptor.ProcessingTimeLogInterceptor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurer  implements WebMvcConfigurer {
+
+//    @Bean
+//    public ProcessingTimeLogInterceptor localeChangeInterceptor() {
+//        ProcessingTimeLogInterceptor lci = new ProcessingTimeLogInterceptor();
+//        lci..setParamName("lang");
+//        return lci;
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(localeChangeInterceptor());
+//    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:errorMessages", "classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
